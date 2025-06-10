20f7bffb6f9b32103323de53ec92b093a5281968
Pietrosor <pietrosorbo@hotmail.it>
2025-06-10 09:45:24 +0200

    Initial commit

---
+++ src/main/java/it/epicode/w5d2u2teoria/W5d2u2teoriaApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.w5d2u2teoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W5d2u2teoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W5d2u2teoriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/w5d2u2teoria/W5d2u2teoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.w5d2u2teoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W5d2u2teoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
